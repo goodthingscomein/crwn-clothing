5017342018e306559aa9176d1c7385562825228a
goodthingscomein <31185022+goodthingscomein@users.noreply.github.com>
2021-04-13 09:50:22 +1000

    added add item and remove item functionality to checkout item component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,7 +1,9 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
 
+    // check if the cart item exists
     const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id);
 
+    // if the cart item exists, we want to increment the quantity
     if(existingCartItem) {
         return cartItems.map(cartItem => 
             cartItem.id === cartItemToAdd.id
@@ -10,5 +12,26 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
             );
     }
 
+    // if the item doesnt exist, we want to add the cart item with a quantity of 1
     return [ ...cartItems, { ...cartItemToAdd, quantity: 1 } ];
+}
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+
+    // check if the cart item exists
+    const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToRemove.id);
+    
+    // if the quantity is 1, we want to remove the item completely
+    if(existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => 
+            cartItem.id !== cartItemToRemove.id
+        )
+    }
+
+    // if the cart item quantity is greater than 1, we want to decrement the quantity
+    return cartItems.map(cartItem => 
+        cartItem.id === cartItemToRemove.id
+            ? { ...cartItem, quantity: cartItem.quantity - 1 }
+            : cartItem
+        );
 }
