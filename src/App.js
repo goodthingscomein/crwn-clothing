a33a3506baf2706996ee9eb6651694817566af9e
goodthingscomein <31185022+goodthingscomein@users.noreply.github.com>
2021-04-13 06:48:07 +1000

    added checkout page, checkout item component and updated cart to use selectors from reselect

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import Header from './components/header/header.component';
 import CheckoutPage from './pages/checkout/checkout.component';
+
+import Header from './components/header/header.component';
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions'
 import { selectCurrentUser } from './redux/user/user.selector';
-import SignIn from './components/sign-in/sign-in.component';
   
 
 class App extends React.Component {
