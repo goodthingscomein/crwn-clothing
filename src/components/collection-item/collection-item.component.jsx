da7fb4f509fa8c1365537253220bec3ec1d059d6
goodthingscomein <31185022+goodthingscomein@users.noreply.github.com>
2021-04-23 10:25:32 +1000

    started updating all of the scss files to jsx

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,27 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import CustomButton from '../custom-button/custom-button.component';
 import { addItem } from '../../redux/cart/cart.actions';
 
-import './collection-item.styles.scss';
+import { CollectionItemContainer, ImageContainer, CustomButtonContainer, 
+    FooterContainer, NameContainer, PriceContainer } from './collection-item.styles.jsx';
 
 const CollectionItem = ({ item, addItem }) => {
     const { name, price, imageUrl } = item;
     return (
-        <div className='collection-item'>
-            <div 
-                className='image'
+        <CollectionItemContainer>
+            <ImageContainer
                 style={{
                     backgroundImage: `url(${imageUrl})`
                 }}
                 />
-            <div className='collection-footer'>
-                <span className='name'>{name}</span>
-                <span className='price'>{price}</span>
-            </div>
-            <CustomButton inverted className='custom-button' onClick={() => addItem(item)}>ADD TO CART</CustomButton>
-        </div>
+            <FooterContainer>
+                <NameContainer>{name}</NameContainer>
+                <PriceContainer>{price}</PriceContainer>
+            </FooterContainer>
+            <CustomButtonContainer inverted onClick={() => addItem(item)}>ADD TO CART</CustomButtonContainer>
+        </CollectionItemContainer>
     );
 
 }
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -10,31 +9,32 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selector';
 
-import './header.styles.scss';
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink } from './header.styles.jsx';
+
 
 const Header = ({ currentUser, hidden }) => (
 
-    <div className='header'>
-        <Link className='logo-container' to='/'>
+    <HeaderContainer>
+        <LogoContainer  to='/'>
             <Logo className='logo' />
-        </Link>
-        <div className='options'>
-            <Link className='option' to='/shop'>
+        </LogoContainer>
+        <OptionsContainer>
+            <OptionLink to='/shop'>
                 SHOP
-            </Link>
-            <Link className='option' to='/shop'>
+            </OptionLink>
+            <OptionLink to='/shop'>
                 CONTACT
-            </Link>
+            </OptionLink>
             {
                 currentUser ?
-                <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div> 
+                <OptionLink as='div' onClick={() => auth.signOut()}>SIGN OUT</OptionLink> 
                 :
-                <Link className='option' to='/signin'>
+                <OptionLink to='/signin'>
                     SIGN IN
-                </Link>
+                </OptionLink>
             }
             <CartIcon />
-        </div>
+        </OptionsContainer>
         {
             hidden 
             ?
@@ -42,7 +42,7 @@ const Header = ({ currentUser, hidden }) => (
             :
             <CartDropdown />
         }
-    </div>
+    </HeaderContainer>
 )
 
 const mapStateToProps = state => createStructuredSelector({
